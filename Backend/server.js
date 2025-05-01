68c7dc6e8663185a379c2e101da95ef1303fcf83
harshkakodkar <harshkakodkar111@gmail.com>
2025-05-02 03:41:33 +0530

    Initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -34,6 +34,9 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Test route is working!' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
