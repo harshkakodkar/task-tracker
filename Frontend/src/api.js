18c7df8cddc202a62268325cb2d355413c1841cd
harshkakodkar <harshkakodkar111@gmail.com>
2025-05-02 03:37:51 +0530

    Initial commit

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { getToken } from './utils/auth';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-tracker-1-496a.onrender.com',
 });
 
 api.interceptors.request.use((req) => {
